ee10f744145551595f64ce19c9da838759afab23
Kirill Tolkachev <lavcraft@gmail.com>
2016-09-24 01:58:25 +0300

    Add analyse tools

--- server/src/main/groovy/ru/ratauth/server/handlers/readers/TokenRequestReader.groovy
+++ server/src/main/groovy/ru/ratauth/server/handlers/readers/TokenRequestReader.groovy
@@ -27,29 +27,33 @@ class TokenRequestReader {
   private static final String REFRESH_TOKEN = "refresh_token"
   private static final String CLIENT_ID = "client_id"
   private static final Set<String> BASE_FIELDS = [RESPONSE_TYPE, SCOPE, GRANT_TYPE, TOKEN, REFRESH_TOKEN] as Set
+  public static final String SPACE = " "
 
   static TokenRequest readTokenRequest(Form form, Headers headers) {
     def auth = extractAuth(headers)
-    GrantType grantType = extractEnumField(form, GRANT_TYPE, true, GrantType.class)
+    GrantType grantType = extractEnumField(form, GRANT_TYPE, true, GrantType)
     AuthAction authAction
     def builder = TokenRequest.builder()
-        .responseType(extractEnumField(form, RESPONSE_TYPE, true, AuthzResponseType.class))
+        .responseType(extractEnumField(form, RESPONSE_TYPE, true, AuthzResponseType))
         .grantType(grantType)
         .clientId(auth[0])
         .clientSecret(auth[1])
-    switch(grantType){
+    switch (grantType) {
       case GrantType.AUTHORIZATION_CODE:
         builder.authzCode(extractField(form, CODE, true))
-        def scope = extractField(form, SCOPE, false)?.split(" ")?.toList()
+        def scope = extractField(form, SCOPE, false)?.split(SPACE)?.toList()
         authAction = AuthAction.TOKEN
-        if(scope)
+        if (scope) {
           builder.scopes(scope)
+        }
         break
-      case GrantType.AUTHENTICATION_TOKEN: builder.scopes(extractField(form, SCOPE, true).split(" ").toList()) //WATCH IT! There is no break here.
+      case GrantType.AUTHENTICATION_TOKEN: builder.scopes(extractField(form, SCOPE, true)
+          .split(SPACE)
+          .toList()) //WATCH IT! There is no break here.
       case GrantType.REFRESH_TOKEN: builder.refreshToken(extractField(form, REFRESH_TOKEN, true))
         authAction = AuthAction.REFRESH_TOKEN
-        break;
-      default: throw new AuthorizationException("Grant type is not supported");
+        break
+      default: throw new AuthorizationException("Grant type is not supported")
     }
     builder.authData(extractRest(form, BASE_FIELDS))
     def request = builder.build()
@@ -58,9 +62,9 @@ class TokenRequestReader {
   }
 
   static CheckTokenRequest readCheckTokenRequest(Form form, Headers headers) {
-    CheckTokenRequest.CheckTokenRequestBuilder builder = CheckTokenRequest.builder().token(extractField(form, TOKEN, true));
+    CheckTokenRequest.CheckTokenRequestBuilder builder = CheckTokenRequest.builder().token(extractField(form, TOKEN, true))
     def auth = extractAuth(headers)
-    builder.clientId(auth[0]).clientSecret(auth[1]);
+    builder.clientId(auth[0]).clientSecret(auth[1])
     builder.externalClientId(extractField(form, CLIENT_ID, false))
     def request = builder.build()
     ActionLogger.addBaseRequestInfo(request.clientId, AuthAction.CHECK_TOKEN)
