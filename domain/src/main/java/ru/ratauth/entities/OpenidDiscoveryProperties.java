45af5e54bca2928be6e09a1ae621e537b4e7f8b1
Sergey Serdyuk <sserdyuk@alfabank.ru>
2017-09-11 13:06:24 +0300

    added builder

--- domain/src/main/java/ru/ratauth/entities/OpenidDiscoveryProperties.java
+++ domain/src/main/java/ru/ratauth/entities/OpenidDiscoveryProperties.java
@@ -1,5 +1,6 @@
 package ru.ratauth.entities;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.List;
  * Created by sserdyuk on 9/8/17.
  */
 @Data
+@Builder
 public class OpenidDiscoveryProperties implements DiscoveryProperties {
     // Required
     private String issuer;
