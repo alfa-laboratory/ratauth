a34fb19aec5e2aeaa68e6302514025ebdc596603
Ruslan Mikhalev <RMikhalev@alfabank.ru>
2017-12-16 16:43:53 +0300

    Remove javaslang dependency

--- server/src/main/groovy/ru/ratauth/server/acr/AcrMatcherWithDefaultClientValues.java
+++ server/src/main/groovy/ru/ratauth/server/acr/AcrMatcherWithDefaultClientValues.java
@@ -4,12 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import ratpack.http.Request;
 import ru.ratauth.entities.AcrValues;
+import ru.ratauth.entities.RelyingParty;
 import ru.ratauth.server.handlers.readers.ReadRequestException;
 import ru.ratauth.services.ClientService;
+import rx.Observable;
 
-import java.util.Optional;
-
-import static javaslang.Tuple.of;
+import java.util.Objects;
 
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class AcrMatcherWithDefaultClientValues implements AcrMatcher {
@@ -20,23 +20,24 @@ public class AcrMatcherWithDefaultClientValues implements AcrMatcher {
     public String match(Request request) {
 
         try {
-            AcrValues defaultAcrValues = clientService
-                    .getRelyingParty(request.getQueryParams().get("client_id"))
-                    .toBlocking().single()
-                    .getDefaultAcrValues();
-
-            String acrValues = Optional
-                    .ofNullable(request.getQueryParams().get("acr_values"))
-                    .orElse(defaultAcrValues != null ? defaultAcrValues.toString() : null);
-
-            return of(acrValues)
+            return Observable.just(request.getQueryParams().get("acr_values"))
+                    .filter(Objects::nonNull)
                     .map(AcrValues::valueOf)
                     .map(AcrValues::getValues)
-                    .map(r -> r.get(0))
-                    ._1();
+                    .map(acr -> acr.get(0))
+                    .switchIfEmpty(fetchDefaultACR(request))
+                    .toBlocking()
+                    .single();
 
         } catch (Exception ex) {
             throw new ReadRequestException("acr values must not be null");
         }
+    }
+
+    private Observable<? extends String> fetchDefaultACR(Request request) {
+        return clientService
+                .getRelyingParty(request.getQueryParams().get("client_id"))
+                .map(RelyingParty::getDefaultAcrValues)
+                .map(Object::toString);
     }
 }
