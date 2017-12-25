689ee5ec327b352d98cd1ff06f1850a0f74663bb
Anton Bashirov <asbashirov@alfabank.ru>
2017-12-25 16:22:40 +0300

    minor refactoring of logging

--- server/src/main/groovy/ru/ratauth/server/handlers/AuthErrorHandler.groovy
+++ server/src/main/groovy/ru/ratauth/server/handlers/AuthErrorHandler.groovy
@@ -37,7 +37,7 @@ class AuthErrorHandler implements ServerErrorHandler {
 
   @Override
   void error(Context context, Throwable throwable) throws Exception {
-    MDC.put("error_message", throwable.message)
+    MDC.put(ERROR_MESSAGE.val(), throwable.message)
     def exception = ExceptionUtils.getThrowable(throwable, BaseAuthServerException, MAX_EXCEPTION_DEPTH)
 
     if (exception instanceof ExpiredException) {
