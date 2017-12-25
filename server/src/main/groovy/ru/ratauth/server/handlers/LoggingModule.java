642075d5aced3fa616caaedeaf761e25a116e446
Anton Bashirov <asbashirov@alfabank.ru>
2017-12-25 15:51:44 +0300

    added logging of device_id for mobile http://jira/browse/PASSPORT-1341

--- server/src/main/groovy/ru/ratauth/server/handlers/LoggingModule.java
+++ server/src/main/groovy/ru/ratauth/server/handlers/LoggingModule.java
@@ -10,26 +10,34 @@ import ratpack.handling.RequestId;
 import ratpack.logging.MDCInterceptor;
 import ru.ratauth.server.services.log.LogFields;
 
+import static ru.ratauth.server.handlers.readers.RequestUtil.extractField;
+import static ru.ratauth.utils.StringUtils.isBlank;
+
 /**
  * @author mgorelikov
  * @since 24/03/16
  */
 @Component
 public class LoggingModule extends AbstractModule {
-  private static final String PERFORATING_TRACE_ID = "trace_id";
+    private static final String PERFORATING_TRACE_ID = "trace_id";
+    private static final String DEVICE_ID = "device_id";
 
-  @Value("${spring.application.name}")
-  private String applicationName;
+    @Value("${spring.application.name}")
+    private String applicationName;
 
-  @Override
-  protected void configure() {
-    bind(RequestId.Generator.class).toInstance(RequestId.Generator.header(PERFORATING_TRACE_ID, RequestId.Generator.randomUuid()));
-    bind(MDCInterceptor.class).toInstance(MDCInterceptor.instance());
-    Multibinder.newSetBinder(binder(), HandlerDecorator.class).addBinding().toInstance(HandlerDecorator.prepend(ctx -> {
-      String requestId = ctx.get(RequestId.class).toString();
-      MDC.put(PERFORATING_TRACE_ID, requestId);
-      MDC.put(LogFields.APPLICATION.val(), applicationName);
-      ctx.next();
-    }));
-  }
+    @Override
+    protected void configure() {
+        bind(RequestId.Generator.class).toInstance(RequestId.Generator.header(PERFORATING_TRACE_ID, RequestId.Generator.randomUuid()));
+        bind(MDCInterceptor.class).toInstance(MDCInterceptor.instance());
+        Multibinder.newSetBinder(binder(), HandlerDecorator.class).addBinding().toInstance(HandlerDecorator.prepend(ctx -> {
+            String requestId = ctx.get(RequestId.class).toString();
+            MDC.put(PERFORATING_TRACE_ID, requestId);
+            String deviceId = extractField(ctx.getRequest().getQueryParams(), DEVICE_ID, false);
+            if (!isBlank(deviceId)) {
+                MDC.put(DEVICE_ID, deviceId);
+            }
+            MDC.put(LogFields.APPLICATION.val(), applicationName);
+            ctx.next();
+        }));
+    }
 }
