5a6b9154650391f9efd06e750992ab992f8c748e
Ruslan Mikhalev <RMikhalev@alfabank.ru>
2018-03-25 11:46:36 +0300

    Reformat code style

--- server/src/main/groovy/ru/ratauth/server/services/OpenIdRegistrationService.java
+++ server/src/main/groovy/ru/ratauth/server/services/OpenIdRegistrationService.java
@@ -27,45 +27,45 @@ import java.util.Map;
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class OpenIdRegistrationService implements RegistrationService {
-  private final Map<String, RegistrationProvider> registerProviders;
-  private final AuthTokenService tokenService;
-  private final AuthSessionService authSessionService;
-  private final AuthClientService authClientService;
+    private final Map<String, RegistrationProvider> registerProviders;
+    private final AuthTokenService tokenService;
+    private final AuthSessionService authSessionService;
+    private final AuthClientService authClientService;
 
-  @Override
-  public Observable<RegistrationResponse> register(RegistrationRequest request) {
-    return authClientService.loadRelyingParty(request.getClientId())
-        .flatMap(rp -> registerProviders.get(rp.getIdentityProvider())
-                .register(RegInput.builder().relyingParty(rp.getName()).data(request.getData()).build())
-                .map(result  -> convertToResponse(result, rp, request))
-        )
-        .doOnCompleted(() -> log.info("First step of registration succeed"));
-  }
-
-  @Override
-  public Observable<TokenResponse> finishRegister(RegistrationRequest request) {
-    return authClientService.loadAndAuthRelyingParty(request.getClientId(), request.getClientSecret(), true)
-        .flatMap(rp -> registerProviders.get(rp.getIdentityProvider())
-                .register(RegInput.builder().relyingParty(rp.getName()).data(request.getData()).build())
-                .map(regResult -> new ImmutablePair<>(rp, regResult)))
-            //TODO@ruslan разделить смс и авторизацию
-        .flatMap(rpRegResult -> authSessionService.createSession(rpRegResult.getLeft(), rpRegResult.getRight().getData(), request.getScopes(), rpRegResult.getRight().getAcrValues(), null)
-                .map(session -> new ImmutablePair<>(rpRegResult.getLeft(), session)))
-        .flatMap(rpSession -> tokenService.createIdTokenAndResponse(rpSession.getRight(), rpSession.getLeft()))
-        .doOnCompleted(() -> log.info("Second step of registration succeed"));
-  }
-
-  private RegistrationResponse convertToResponse(RegResult regResult, RelyingParty relyingParty, RegistrationRequest request) {
-    String redirectURI = request.getRedirectURI();
-    if(StringUtils.isBlank(redirectURI)) {
-      redirectURI = relyingParty.getRegistrationRedirectURI();
-    } else {
-      if(!URIUtils.compareHosts(redirectURI, relyingParty.getRedirectURIs()))
-        throw new RegistrationException(RegistrationException.ID.REDIRECT_NOT_CORRECT);
+    @Override
+    public Observable<RegistrationResponse> register(RegistrationRequest request) {
+        return authClientService.loadRelyingParty(request.getClientId())
+                .flatMap(rp -> registerProviders.get(rp.getIdentityProvider())
+                        .register(RegInput.builder().relyingParty(rp.getName()).data(request.getData()).build())
+                        .map(result -> convertToResponse(result, rp, request))
+                )
+                .doOnCompleted(() -> log.info("First step of registration succeed"));
     }
-    return RegistrationResponse.builder()
-        .data(regResult.getData())
-        .redirectUrl(redirectURI)
-        .build();
-  }
+
+    @Override
+    public Observable<TokenResponse> finishRegister(RegistrationRequest request) {
+        return authClientService.loadAndAuthRelyingParty(request.getClientId(), request.getClientSecret(), true)
+                .flatMap(rp -> registerProviders.get(rp.getIdentityProvider())
+                        .register(RegInput.builder().relyingParty(rp.getName()).data(request.getData()).build())
+                        .map(regResult -> new ImmutablePair<>(rp, regResult)))
+                //TODO@ruslan разделить смс и авторизацию
+                .flatMap(rpRegResult -> authSessionService.createSession(rpRegResult.getLeft(), rpRegResult.getRight().getData(), request.getScopes(), rpRegResult.getRight().getAcrValues(), null)
+                        .map(session -> new ImmutablePair<>(rpRegResult.getLeft(), session)))
+                .flatMap(rpSession -> tokenService.createIdTokenAndResponse(rpSession.getRight(), rpSession.getLeft()))
+                .doOnCompleted(() -> log.info("Second step of registration succeed"));
+    }
+
+    private RegistrationResponse convertToResponse(RegResult regResult, RelyingParty relyingParty, RegistrationRequest request) {
+        String redirectURI = request.getRedirectURI();
+        if (StringUtils.isBlank(redirectURI)) {
+            redirectURI = relyingParty.getRegistrationRedirectURI();
+        } else {
+            if (!URIUtils.compareHosts(redirectURI, relyingParty.getRedirectURIs()))
+                throw new RegistrationException(RegistrationException.ID.REDIRECT_NOT_CORRECT);
+        }
+        return RegistrationResponse.builder()
+                .data(regResult.getData())
+                .redirectUrl(redirectURI)
+                .build();
+    }
 }
