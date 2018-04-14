8d19a307a9b258e7ea27a1695845b9c258b63f4c
Ruslan Mikhalev <RMikhalev@alfabank.ru>
2018-04-14 14:46:14 +0300

    Add simple DeviceInfo

---
+++ domain/src/main/java/ru/ratauth/entities/DeviceInfo.java
@@ -0,0 +1,14 @@
+package ru.ratauth.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeviceInfo {
+    private String userId;
+    private String deviceId;
+}
---
+++ server/src/main/groovy/ru/ratauth/server/services/DeviceInfoService.java
@@ -0,0 +1,9 @@
+package ru.ratauth.server.services;
+
+import ru.ratauth.entities.DeviceInfo;
+
+public interface DeviceInfoService {
+
+    void create(DeviceInfo deviceInfo);
+
+}
