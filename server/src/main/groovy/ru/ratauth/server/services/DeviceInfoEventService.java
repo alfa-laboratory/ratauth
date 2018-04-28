8d34b44cd527293e79fd4cab3e7c4fe0fb9cf7c6
Ruslan Mikhalev <RMikhalev@alfabank.ru>
2018-04-28 15:40:11 +0300

    Add interface

---
+++ server/src/main/groovy/ru/ratauth/server/services/DeviceInfoEventService.java
@@ -0,0 +1,10 @@
+package ru.ratauth.server.services;
+
+import ru.ratauth.entities.DeviceInfo;
+import rx.Observable;
+
+public interface DeviceInfoEventService {
+
+    Observable<DeviceInfo> sendChangeDeviceInfoEvent(String clientId, String enroll, DeviceInfo oldDeviceInfo, DeviceInfo deviceInfo);
+
+}
