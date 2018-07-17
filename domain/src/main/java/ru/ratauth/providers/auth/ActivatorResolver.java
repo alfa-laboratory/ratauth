ad2162ea5276a5d06a20d4af4e0bd795326ffa83
Ruslan Mikhalev <RMikhalev@alfabank.ru>
2018-07-17 14:46:11 +0300

    Refactor map instantiation

--- domain/src/main/java/ru/ratauth/providers/auth/ActivatorResolver.java
+++ domain/src/main/java/ru/ratauth/providers/auth/ActivatorResolver.java
@@ -1,9 +1,8 @@
 package ru.ratauth.providers.auth;
 
-import lombok.extern.slf4j.Slf4j;
 import ru.ratauth.exception.MissingProviderException;
 
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -15,7 +14,7 @@ public class ActivatorResolver {
     private final Map<String, Activator> activators;
 
     public ActivatorResolver() {
-        this.activators = new HashMap<>();
+        this.activators = Collections.emptyMap();
     }
 
     public ActivatorResolver(List<Activator> activators) {
