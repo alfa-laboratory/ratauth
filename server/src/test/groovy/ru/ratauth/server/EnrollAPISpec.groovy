6542f137497d5d40ec8f595f2782c4b5dbac0bc6
Sergey Serdyuk <sserdyuk@alfabank.ru>
2018-07-17 15:03:26 +0300

    add some test for auth code update expired

--- server/src/test/groovy/ru/ratauth/server/EnrollAPISpec.groovy
+++ server/src/test/groovy/ru/ratauth/server/EnrollAPISpec.groovy
@@ -8,20 +8,14 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.restdocs.payload.JsonFieldType
-import ru.ratauth.interaction.AuthzResponseType
-import ru.ratauth.interaction.GrantType
+import ru.ratauth.exception.AuthorizationException
 import ru.ratauth.server.local.PersistenceServiceStubConfiguration
 
 import static com.jayway.restassured.RestAssured.given
-import static java.net.URLEncoder.*
-import static org.hamcrest.Matchers.contains
-import static org.hamcrest.Matchers.containsInAnyOrder
+import static java.net.URLEncoder.encode
 import static org.hamcrest.Matchers.equalTo
-import static org.hamcrest.Matchers.equalToIgnoringCase
 import static org.hamcrest.Matchers.notNullValue
 import static org.hamcrest.Matchers.startsWith
-import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName
-import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath
@@ -274,5 +268,48 @@ class EnrollAPISpec extends BaseDocumentationSpec {
                 .header(HttpHeaders.LOCATION, StringContains.containsString("code=code"))
                 .header(HttpHeaders.LOCATION, StringContains.containsString("session_token=session_token"))
                 .header(HttpHeaders.LOCATION, StringContains.containsString('https://domain.mine/login?session_token=session_token&code=code&expires_in=36000'))
+    }
+
+    def 'verify enroll final step exception update auth code expires_in'() {
+        given:
+        def setup = given(this.documentationSpec)
+                .accept(ContentType.URLENC)
+                .filter(document('mfa_activate_succeed',
+                preprocessResponse(prettyPrint()),
+                requestParameters(
+                        parameterWithName('mfa_token')
+                                .description('MFA token issued for permission update'),
+                        parameterWithName('redirect_uri')
+                                .description('Redirect URL'),
+                        parameterWithName('client_id')
+                                .description('Relying party identifier'),
+                        parameterWithName('username')
+                                .description('part of user\'s credentials'),
+                        parameterWithName('password')
+                                .description('part of user\'s credentials'),
+                        parameterWithName('scope')
+                                .description('Scope for authorization that will be provided through JWT to all resource servers in flow'),
+                        parameterWithName('acr_values')
+                                .description('Authentication Context Class Reference'),
+                        parameterWithName('enroll')
+                                .description('Required Authentication Context Class Reference'),
+
+                )
+        ))
+                .given()
+                .formParam("username", "username")
+                .formParam("password", "password")
+                .formParam("redirect_uri", "https://domain.mine/login")
+                .formParam('mfa_token', PersistenceServiceStubConfiguration.MFA_TOKEN_2)
+                .formParam('client_id', PersistenceServiceStubConfiguration.CLIENT_NAME)
+                .formParam('scope', 'rs.read')
+                .formParam('acr_values', 'username')
+                .formParam('enroll', 'username')
+        when:
+        def result = setup
+                .when()
+                .post("verify")
+        then:
+            result.then().extract().jsonPath().get("id") == AuthorizationException.ID.AUTH_CODE_NOT_FOUND.name()
     }
 }
