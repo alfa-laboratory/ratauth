65ee3c875dbb83389d4ceaf283c23125ed2d133c
Elizaveta Andronova <EAndronova@alfabank.ru>
2018-07-26 13:29:40 +0300

    added logging

--- server/src/main/groovy/ru/ratauth/server/providers/RestIdentityProvider.groovy
+++ server/src/main/groovy/ru/ratauth/server/providers/RestIdentityProvider.groovy
@@ -5,6 +5,7 @@ import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+import ratpack.http.client.HttpClient
 import ratpack.http.client.ReceivedResponse
 import ru.ratauth.entities.AcrValues
 import ru.ratauth.entities.IdentityProvider
@@ -29,6 +30,7 @@ class RestIdentityProvider implements IdentityProvider {
     @Autowired
     IdentityProvidersConfiguration identityProvidersConfiguration
 
+
     String name() {
         return "REST"
     }
@@ -38,7 +40,9 @@ class RestIdentityProvider implements IdentityProvider {
         String enroll = input.enroll.first
         DestinationConfiguration config = identityProvidersConfiguration.idp?.get(enroll)?.activate
 
-        log.info("Sending request to ${enroll}")
+        log.info("data " +input.data + " userInfo " + input.userInfo + " relyingParty " + input.relyingParty +  " enroll " + enroll +" config " + config)
+        log.info("Sending activate request to ${enroll}")
+        log.info("HUITA" + HttpClientHolder.instance)
         return new HystrixIdentityProviderCommand(
                 HttpClientHolder.instance,
                 input.data,
@@ -50,7 +54,9 @@ class RestIdentityProvider implements IdentityProvider {
                 config?.authPassword
         )
                 .toObservable()
-                .map({ ReceivedResponse res -> makeActivateResultFromResponse(res)
+                .map({ ReceivedResponse res ->
+            log.info("YA VNUTRY BLOKA AZAZAZA      _______ " + res.body.text)
+            makeActivateResultFromResponse(res)
         })
     }
 
@@ -58,8 +64,8 @@ class RestIdentityProvider implements IdentityProvider {
     Observable<VerifyResult> verify(VerifyInput input) {
         String enroll = input.enroll.first
         DestinationConfiguration config = identityProvidersConfiguration.idp?.get(enroll)?.verify
-
-        log.info("Sending request to ${enroll}")
+        log.info("data " +input.data + " userInfo " + input.userInfo + " relyingParty " + input.relyingParty +  " enroll " + enroll)
+        log.info("Sending verify request to ${enroll}")
         return new HystrixIdentityProviderCommand(
                 HttpClientHolder.instance,
                 input.data,
@@ -83,7 +89,9 @@ class RestIdentityProvider implements IdentityProvider {
     }
 
     private static VerifyResult makeVerifyResultFromResponse(ReceivedResponse receivedResponse) {
-        def response = new JsonSlurper().parseText(receivedResponse.body.text) as Map
+        log.info("TRATATATATTA " + receivedResponse.body.text)
+        def response = (new JsonSlurper().parseText(receivedResponse.body.text)  as List)[0] as Map
+        log.info("RESPOOOOOONSE " + response + "\nDATAAAAAAA" + response.data)
         assert response.data[Fields.USER_ID.val()]
         new VerifyResult(
             data:response.data as Map,
@@ -100,6 +108,7 @@ class RestIdentityProvider implements IdentityProvider {
     }
 
     private static ActivateResult makeActivateResultFromResponse(ReceivedResponse receivedResponse) {
+        log.info("___________________________\n " + receivedResponse.body.text + "\n_____________")
         def response = new JsonSlurper().parseText(receivedResponse.body.text) as Map
         assert response.data[Fields.USER_ID.val()]
         return new ActivateResult(response.data as Map)
