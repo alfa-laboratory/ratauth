30cc56fc7ff98354a9597143e453dce27ce49475
Ruslan Mikhalev <RMikhalev@alfabank.ru>
2018-09-07 10:25:54 +0300

    Remove author

--- server/src/main/groovy/ru/ratauth/server/configuration/SessionConfiguration.java
+++ server/src/main/groovy/ru/ratauth/server/configuration/SessionConfiguration.java
@@ -3,10 +3,6 @@ package ru.ratauth.server.configuration;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-/**
- * @author tolkv
- * @version 09/11/2016
- */
 @Data
 @ConfigurationProperties(prefix = "auth.session")
 public class SessionConfiguration {
