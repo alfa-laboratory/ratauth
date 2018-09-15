8ac10df976d45114d67ae782c68f1140ea8f0248
Sergey Serdyuk <sserdyuk@alfabank.ru>
2018-09-15 17:31:17 +0300

    extract call external service

--- server/src/main/groovy/ru/ratauth/server/handlers/UpdateHandler.groovy
+++ server/src/main/groovy/ru/ratauth/server/handlers/UpdateHandler.groovy
@@ -9,6 +9,7 @@ import ratpack.func.Action
 import ratpack.handling.Chain
 import ratpack.handling.Context
 import ru.ratauth.entities.AuthEntry
+import ru.ratauth.entities.UpdateDataEntry
 import ru.ratauth.exception.AuthorizationException
 import ru.ratauth.interaction.UpdateServiceRequest
 import ru.ratauth.server.extended.update.UpdateFinishResponse
@@ -58,36 +59,42 @@ class UpdateHandler implements Action<Chain> {
 
     private void updateUserData(UpdateServiceRequest request, Context ctx) {
         updateDataService.getValidEntry(request.code)
-            .subscribe {
-                data ->
+                .subscribe { data ->
                     updateDataService.invalidate(request.code).subscribe()
-                    updateService.update(UpdateServiceInput.builder()
-                            .code(request.code)
-                            .updateService(request.updateService)
-                            .relyingParty(request.clientId)
-                            .data(request.data).build())
-                            .flatMap { r ->
-                                def clientId = request.clientId
-                                def sessionToken = data.sessionToken
-                                doFinishResponse(ctx, clientId, sessionToken) }
-                            .doOnError { throwable -> ctx.get(ServerErrorHandler).error(ctx, throwable) }
-                            .subscribe()
-        } { throwable -> ctx.get(ServerErrorHandler).error(ctx, throwable) }
+                    callExternalServiceForUpdateUserData(data, ctx)
+                    doFinishResponse(ctx, request, data)
+                } { throwable -> ctx.get(ServerErrorHandler).error(ctx, throwable) }
     }
 
-    private void doFinishResponse(Context ctx, String clientId, String sessionToken) {
+    private void callExternalServiceForUpdateUserData(UpdateServiceRequest request, UpdateDataEntry data, Context ctx) {
+        if (!request.skip && !data.required) {
+            updateService.update(UpdateServiceInput.builder()
+                    .code(request.code)
+                    .updateService(request.updateService)
+                    .relyingParty(request.clientId)
+                    .data(request.data).build())
+                    .doOnError { throwable -> ctx.get(ServerErrorHandler).error(ctx, throwable) }
+                    .subscribe()
+        }
+    }
+
+    private void doFinishResponse(Context ctx, UpdateServiceRequest request, UpdateDataEntry data) {
+
+        def clientId = request.clientId
+        def sessionToken = data.sessionToken
         LocalDateTime now = now()
+
         authClientService.loadRelyingParty(clientId)
-            .zipWith(getSession(sessionToken, clientId), { relyingParty, authEntry ->
-                String authCode = authEntry.authCode
-                LocalDateTime authCodeExpiresIn = now.plus(relyingParty.codeTTL, SECONDS)
+                .zipWith(getSession(sessionToken, clientId), { relyingParty, authEntry ->
+            String authCode = authEntry.authCode
+            LocalDateTime authCodeExpiresIn = now.plus(relyingParty.codeTTL, SECONDS)
 
-                updateAuthCodeExpired(authCode, authCodeExpiresIn)
+            updateAuthCodeExpired(authCode, authCodeExpiresIn)
 
-                long expiresIn = SECONDS.between(now, authCodeExpiresIn)
+            long expiresIn = SECONDS.between(now, authCodeExpiresIn)
 
-                def finishResponse = new UpdateFinishResponse(relyingParty.authorizationRedirectURI, sessionToken, authCode, expiresIn)
-                ctx.redirect(FOUND.code(), finishResponse.redirectURL)
+            def finishResponse = new UpdateFinishResponse(relyingParty.authorizationRedirectURI, sessionToken, authCode, expiresIn)
+            ctx.redirect(FOUND.code(), finishResponse.redirectURL)
         }).subscribe()
     }
 
