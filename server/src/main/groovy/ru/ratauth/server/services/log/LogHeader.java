16964f7a24c3827a65debce32e223f960bc745be
AZamakhaev <o>
2020-02-26 14:25:07 +0300

    + x-b3-traceid forwarding

--- server/src/main/groovy/ru/ratauth/server/services/log/LogHeader.java
+++ server/src/main/groovy/ru/ratauth/server/services/log/LogHeader.java
@@ -2,16 +2,19 @@ package ru.ratauth.server.services.log;
 
 import lombok.RequiredArgsConstructor;
 
+/**
+ * Forwarding MDC values to request headers
+ */
 @RequiredArgsConstructor
 public enum LogHeader {
     CLIENT_ID("Client-Id", "client_id"),
     CLIENT_IP("Client-Ip", "client_ip"),
-    USER_ID("User-Id", "user_id"),
     DEVICE_ID("Device-Id", "device_id"),
     SESSION_ID("Session-Id", "session_id"),
     TRACE_ID("Trace-Id", "trace_id"),
-    X_B3_TRACE_ID("X-B3-TraceId", "X-B3-TraceId"),
-    X_B3_SPAN_ID("X-B3-SpanId", "X-B3-SpanId");
+    USER_ID("User-Id", "user_id"),
+    X_B3_SPAN_ID("X-B3-SpanId", "X-B3-SpanId"),
+    X_B3_TRACE_ID("X-B3-TraceId", "X-B3-TraceId");
 
     private final String headerValue;
     private final String mdcValue;
@@ -19,6 +22,7 @@ public enum LogHeader {
     public String headerVal() {
         return headerValue;
     }
+
     public String mdcVal() {
         return mdcValue;
     }
