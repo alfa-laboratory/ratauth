407371d186b66e4575c6b4e14c94ae4b218b289e
Aleksey Tokar <atokar2@alfabank.ru>
2020-04-02 20:20:17 +0300

    remove unnecessery classes

--- server/src/test/groovy/ru/ratauth/server/configuration/TestBaseConfiguration.java
+++ server/src/test/groovy/ru/ratauth/server/configuration/TestBaseConfiguration.java
@@ -3,13 +3,14 @@ package ru.ratauth.server.configuration;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.TestPropertySource;
+import ru.ratauth.server.local.PersistenceServiceStubConfiguration;
 
 /**
  * @author tolkv
  * @version 23/09/16
  */
 @TestConfiguration
-@Import({TestProvidersStubConfiguration.class, TestPersistenceServiceStubConfiguration.class})
+@Import({TestProvidersStubConfiguration.class, PersistenceServiceStubConfiguration.class})
 @TestPropertySource(properties = {
         "ratpack.port=8080",
         "ratpack.base-dir=file:server/src/main/resources",
